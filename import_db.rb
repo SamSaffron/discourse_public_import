876a77aa24de2db1be94d24b76870cd7e5fee571
Sam Saffron <sam.saffron@gmail.com>
2023-05-11 17:03:09 +1000

    work in progress

--- import_db.rb
+++ import_db.rb
@@ -7,22 +7,77 @@ conn = MiniSql::Connection.get(sqlite_conn)
 Dir.chdir("/home/sam/Source/discourse")
 require "/home/sam/Source/discourse/config/environment"
 
-puts "Importing users..."
+RateLimiter.disable
 
-created = 0
-conn
-  .query("SELECT * FROM users")
-  .each do |row|
-    if !User.exists?(row.id)
-      User.create(
-        id: row.id,
-        username: row.username,
-        name: row.username,
-        password: SecureRandom.hex,
-        email: "#{SecureRandom.hex}@email.com"
-      )
+def import_users(conn)
+  puts "Importing users..."
+  created = 0
+  conn
+    .query("SELECT * FROM users")
+    .each_slice(5000) do |slice|
+      slice.each do |row|
+        User.transaction do
+          if !User.exists?(row.id)
+            User.create(
+              id: row.id,
+              username: row.username,
+              name: row.username,
+              password: SecureRandom.hex,
+              email: "#{SecureRandom.hex}@email.com"
+            )
+          end
+          print "."
+          created += 1
+          puts "#{created} users created" if created % 500 == 0
+        end
+      end
     end
-    print "."
-    created += 1
-    puts "#{created} users created" if created % 500 == 0
-  end
+end
+
+def import_topics(conn)
+  categories =
+    Category.pluck(:name, :id).map { |name, id| [name.downcase, id] }.to_h
+
+  puts "creating topics..."
+
+  created = 0
+  conn
+    .query("SELECT id FROM topics")
+    .each_slice(100) do |slice|
+      Topic.transaction do
+        slice.each do |row|
+          if !Topic.exists?(row.id)
+            topic =
+              conn.query(
+                "SELECT * FROM topics t JOIN posts p on p.topic_id = t.id WHERE t.id = ?",
+                row.id
+              ).first
+
+            if !categories[topic.category.downcase]
+              category = Category.create!(name: topic.category, user_id: -1)
+              categories[topic.category.downcase] = category.id
+              puts "created #{topic.category}"
+            end
+
+            t =
+              Topic.new(
+                id: topic.id,
+                title: topic.title,
+                category_id: categories[topic.category],
+                user_id: topic.user_id,
+                created_at: topic.created_at,
+                updated_at: topic.created_at
+              )
+            t.save!(validate: false)
+            print "."
+          end
+
+          created += 1
+          puts "#{created} topics created" if created % 500 == 0
+        end
+      end
+    end
+end
+# import_users(conn)
+import_topics(conn)
+#
